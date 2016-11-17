154da406e1927ebcf96d33004b27b23c874dd0d9
Jonathan Gamble <jon@jongamble.com>
2016-11-16 23:23:00 -0500

    Initial push.

---
+++ src/components/IndexPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IndexData from '../data/index-page';
+
+export default class IndexPage extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        
+      </div>
+    );
+  }
+}
---
+++ src/data/index-page.js
@@ -0,0 +1,6 @@
+const indexData = {
+  'id': 'index',
+  'title': 'Jon Gamble and Kate McKee',
+};
+
+export default indexData;
