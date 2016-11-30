7be25bcb4cc56766da90fd1829796bcf396baec8
Jonathan Gamble <jgamble@us.ibm.com>
2016-11-30 15:00:19 -0500

    adding data schema

--- src/data/index-page.js
+++ src/data/index-page.js
@@ -1,6 +1,125 @@
 const indexData = {
-  'id': 'index',
-  'title': 'Jon Gamble and Kate McKee',
+    'header': {
+        'location': 'Banchory, Scotland',
+        'date': 'July 12, 2017',
+        'leftName': 'Jon Gamble',
+        'rightName': 'Kate McKee',
+        'image': ''
+    },
+    'about': {
+        'bride': {
+            'headline': '',
+            'copy': '',
+            'image': ''
+        },
+        'groom': {
+            'headline': '',
+            'copy': '',
+            'image': ''
+        },
+        'couple': {
+            'headline': '',
+            'copy': '',
+            'image': ''
+        }
+    },
+    'travel': [
+        {
+            'headline': '',
+            'copy': ''
+        },
+        {
+            'headline': '',
+            'copy': ''
+        },
+        {
+            'headline': '',
+            'copy': ''
+        }
+    ],
+    'events': [
+        {
+            'headline': '',
+            'copy': ''
+        },
+        {
+            'headline': '',
+            'copy': ''
+        },
+        {
+            'headline': '',
+            'copy': ''
+        }
+    ],
+    'rsvp': {
+        'headline': '',
+        'copy': '',
+        'invitationNames': [
+            {
+                'name': '',
+                'numberInvited': 1
+            },
+            {
+                'name': '',
+                'numberInvited': 1
+            },
+            {
+                'name': '',
+                'numberInvited': 1
+            }
+        ],
+        'formFields': [
+            'name',
+            'email',
+            'food',
+            'attending'
+        ]
+    },
+    'contact': [
+        {
+            'name': '',
+            'phone': '',
+            'email': ''
+        },
+        {
+            'name': '',
+            'phone': '',
+            'email': ''
+        }
+    ],
+    'registry': [
+        {
+            'store': '',
+            'link': '',
+            'image': ''
+        },
+        {
+            'store': '',
+            'link': '',
+            'image': ''
+        },
+        {
+            'store': '',
+            'link': '',
+            'image': ''
+        }
+    ],
+    'bridalParty': {
+        'groom': [
+            {
+                'name': '',
+                'copy': '',
+                'image': ''
+            }
+        ],
+        'bride': [
+            {
+                'name': '',
+                'copy': '',
+                'image': ''
+            }
+        ]
+    }
 };
 
 export default indexData;
